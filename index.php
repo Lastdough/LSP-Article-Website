59c950c4708629043865e66bdda072c8f208ffa8
Abdurr <94680006+Lastdough@users.noreply.github.com>
2024-01-02 22:04:06 +0700

    [UPDATE] Routing more concise

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 
 require_once 'src\app\controllers\ArticleController.php';
@@ -20,8 +21,9 @@ function json(mixed $data)
 function getUri(): string
 {
     $uri = $_SERVER['REQUEST_URI'];
+
     // Base path in htdocs
-    $basePath = '/LSPWebsite'; 
+    $basePath = '/LSPWebsite';
 
     // Remove base path from URI
     if (substr($uri, 0, strlen($basePath)) == $basePath) {
@@ -34,59 +36,83 @@ function getUri(): string
     return $uri;
 }
 
-$request = getUri();
+$uri = getUri();
+$method = $_SERVER['REQUEST_METHOD'];
 
-switch ($request) {
+switch ($uri) {
     case '/':
-        $db = new Database();
-        $db->getConnection();
-
-        $article = new Article();
-        $data = $article->getAllArticles();
-        json([
-            'msg' => 'Hello!',
-            'data' => $data
-        ]);
+        $articleController = new ArticleController();
+        $articleController->index();
         break;
     case '/article':
         if (isset($_GET['id'])) {
             $articleController = new ArticleController();
-            // $a = $articleController->view($_GET['id']);
+            $articleController->view($_GET['id']);
         } else {
-            json([
-                'msg' => 'Article',
-                'data' => 'no id'
-            ]);
+            $articleController = new ArticleController();
+            $articleController->not_found_404();
         }
-        json([
-            'msg' => 'Article',
-            'data' => $_GET["id"]
-        ]);
         break;
     case '/admin':
-
+        $adminController = new AdminController();
+        $adminController->index();
         break;
     case '/admin/register':
-        $adminController = new AdminController();
-        $adminController->register();
+        if ($method == 'GET') {
+            $adminController = new AdminController();
+            $adminController->registerView();
+        } else if ($method == 'POST') {
+            $adminController = new AdminController();
+            $adminController->register();
+        } else {
+            $articleController = new ArticleController();
+            $articleController->not_found_404();
+        }
         break;
     case '/admin/login':
-
+        if ($method == 'GET') {
+            $adminController = new AdminController();
+            $adminController->loginView();
+        } else if ($method == 'POST') {
+            $adminController = new AdminController();
+            $adminController->login($_POST['username'], $_POST['password']);
+        } else {
+            $articleController = new ArticleController();
+            $articleController->not_found_404();
+        }
+        break;
+    case '/admin/logout':
+        $adminController = new AdminController();
+        $adminController->logout();
         break;
     case '/admin/dashboard':
-
+        $adminController = new AdminController();
+        $adminController->dashboard();
         break;
     case '/admin/create':
-
+        if ($method == 'GET') {
+            $articleController = new ArticleController();
+            $articleController->createArticleView();
+        } else if ($method == 'POST') {
+            $articleController = new ArticleController();
+            $articleController->createArticle();
+        } else {
+            $articleController = new ArticleController();
+            $articleController->not_found_404();
+        }
         break;
     case '/admin/edit':
-
+        if (isset($_GET['id'])) {
+            $articleController = new ArticleController();
+            $articleController->view($_GET['id']);
+        } else {
+            $articleController = new ArticleController();
+            $articleController->not_found_404();
+        }
         break;
     default:
+        $articleController = new ArticleController();
+        $articleController->not_found_404();
         // 404 Not Found
-        json([
-            'msg' => '404',
-            'data' => $_SERVER['REQUEST_URI'] . " | ".$request
-        ]);
         break;
 }
