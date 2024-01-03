ff94087e304e9b330a5efb923a2a022e5da98bdb
Abdurr <94680006+Lastdough@users.noreply.github.com>
2024-01-04 02:42:12 +0700

    side nav

--- src/app/views/admin/dashboard.php
+++ src/app/views/admin/dashboard.php
@@ -5,31 +5,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="src\public\css\style.css">
+    <!-- Include Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://rsms.me/">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
 </head>
 
-<body>
-    <h1>Dashboard</h1>
-    <!-- component -->
-    <!-- This is an example component -->
-    <!-- <div class="pt-2 relative text-gray-600">
-        <form action="">
-            <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                type="search" name="search" placeholder="Search">
-            <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
-                <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
-                    viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
-                    width="512px" height="512px">
-                    <path
-                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                </svg>
-            </button>
-        </form>
-    </div> -->
+<body class="bg-gray-200 flex">
+    <aside class="bg-white min-h-screen shadow-md">
+        <nav class="text-sm">
+            <div class="flex h-screen flex-col justify-between pt-2 pb-6">
+                <div>
+                    <ul class="mt-5 space-y-2 tracking-wide">
+                        <li>
+                            <a href="/LSPWebsite/admin/dashboard" aria-label="authorname" class="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-black">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                                    <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-240v-32q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v32q0 33-23.5 56.5T720-160H240q-33 0-56.5-23.5T160-240Z" />
+                                </svg>
+                                <h2 class="text-2xl font-bold">Author Name</h2>
+                            </a>
+                        </li>
+                        <li class="min-w-max">
+                            <a href="/LSPWebsite/admin/dashboard" aria-label="dashboard" class="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-black">
+                                <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                                    <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" class="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                                    <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                                    <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" class="fill-current group-hover:text-sky-300"></path>
+                                </svg>
+                                <span class="-mr-1 font-medium">Dashboard</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="w-max -mb-3">
+                    <a href="/LSPWebsite/admin/logout" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-red-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:text-red-800" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor">
+                            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+                        </svg>
+                        <span class="group-hover:text-red-800 font-medium">Log Out</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </aside>
+    <main class="flex">
+        <div class="p-6 container mx-auto">
+            <div class="flex justify-between items-center pb-4">
+                <h2 class="text-2xl font-bold">Articles</h2>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors">Add Article</button>
+            </div>
+
+            <div class="flex">
+                <table class="bg-white">
+                    <thead class="bg-gray-800 text-white">
+                        <tr>
+                            <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Date</th>
+                            <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Title</th>
+                            <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Description</th>
+                            <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Status</th>
+                            <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-700">
+                        <tr>
+                            <td class="text-left py-3 px-4">12 July 2021</td>
+                            <td class="text-left py-3 px-4">Another article</td>
+                            <td class="text-left py-3 px-4">Lorem Lorem</td>
+                            <td class="text-left py-3 px-4">PUBLISHED</td>
+                            <td class="flex items-center py-3 px-4">
+                                <button class="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-700 transition-colors mr-2">Edit</button>
+                                <button class="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700 transition-colors">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
