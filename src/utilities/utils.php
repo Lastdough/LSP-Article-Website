57d09e3b2eb424e7b984b74d10abe944d0e873b9
Abdurr <94680006+Lastdough@users.noreply.github.com>
2024-01-05 17:14:56 +0700

    misc

--- src/utilities/utils.php
+++ src/utilities/utils.php
@@ -3,15 +3,3 @@ function isAdminLoggedIn()
 {
     return isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true;
 }
-
-function generateArticleId($pdo, $adminId)
-{
-    $datePart = date('Ymd');
-    // Query to get the count of articles for today by this admin
-    $stmt = $pdo->prepare("SELECT COUNT(*) FROM article_table WHERE admin_id = ? AND DATE(created_at) = CURDATE()");
-    $stmt->execute([$adminId]);
-    $todayCount = $stmt->fetchColumn() + 1; // Add 1 to get the next sequence number
-
-    $articleId = $datePart . $adminId . str_pad($todayCount, 4, '0', STR_PAD_LEFT);
-    return $articleId;
-}
