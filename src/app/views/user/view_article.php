105b6a26b9f3806b9082555983342f91b7a2e134
Abdurr <94680006+Lastdough@users.noreply.github.com>
2024-01-06 17:46:38 +0700

    [FIX] bug on title

--- src/app/views/user/view_article.php
+++ src/app/views/user/view_article.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php if ($article) : ?>
-        <title><?= $article['title'] ?></title>
+        <title><?= htmlspecialchars($article['title']) ?></title>
     <?php else : ?>
         <title>Article not found.</title>
     <?php endif; ?>
@@ -41,9 +41,9 @@
 <body class="bg-gray-100">
 
     <div class="container mx-auto my-10 p-8 bg-white shadow-lg rounded-lg">
-        <h1 class="text-4xl font-bold mb-4"><?= $article['title'] ?></h1>
+        <h1 class="text-4xl font-bold mb-4"><?= htmlspecialchars($article['title']) ?></h1>
         <h2 class="text-2xl font-semibold mb-4"><?= htmlspecialchars($article['header']) ?></h2>
-        <p class="text-gray-600 mb-2">By <?= $article['author_name'] ?> | <?= $formattedDate ?></p>
+        <p class="text-gray-600 mb-2">By <?= htmlspecialchars($article['author_name']) ?> | <?= $formattedDate ?></p>
 
         <div class="border-b border-t pt-2 pb-1 border-gray-300">
             <!-- Article Image -->
